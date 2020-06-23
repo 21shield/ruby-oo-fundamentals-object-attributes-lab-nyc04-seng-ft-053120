b4847edb619c8eaead82d88835f2b0357a296c14
21shield <21netaly@gmail.com>
2020-06-22 22:01:36 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def name= (name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def job= (ocupation)
+        @job = ocupation
+    end
+    def job
+        @job
+    end
+end
